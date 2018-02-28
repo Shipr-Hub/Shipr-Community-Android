948def9d2bfd5910841200980e6a3549c60d600d
Yash <yhdesai99@ymail.com>
2018-03-01 03:54:56 +0530

    removed phone sign-in

--- app/src/main/java/io/github/yhdesai/devprops/Fragments/general.java
+++ app/src/main/java/io/github/yhdesai/devprops/Fragments/general.java
@@ -176,10 +176,7 @@ public void onClick(View view) {
                                                  //   new AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build(),
                                                     new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
                                                 //    new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build(),
-                                                    new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-                                                    new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build()
-
-
+                                                    new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build()
                                             ))
                                     .build(),
                             RC_SIGN_IN);
