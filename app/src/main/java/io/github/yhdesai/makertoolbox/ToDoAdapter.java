0c1a7f00fd66c4e13d1c447cfa2da07219023bba
Yash <yhdelectric@gmail.com>
2018-04-17 02:40:41 +0530

    Added labels to ToDo

--- app/src/main/java/io/github/yhdesai/makertoolbox/ToDoAdapter.java
+++ app/src/main/java/io/github/yhdesai/makertoolbox/ToDoAdapter.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
 
 public class ToDoAdapter extends ArrayAdapter<DeveloperToDo> {
@@ -22,11 +24,27 @@ public class ToDoAdapter extends ArrayAdapter<DeveloperToDo> {
         }
         TextView titleTextView = convertView.findViewById(R.id.titleTextView);
         TextView descTextView = convertView.findViewById(R.id.descTextView);
+        TextView featureTextView = convertView.findViewById(R.id.feature);
+        TextView bugTextView = convertView.findViewById(R.id.bug);
 
         DeveloperToDo todo = getItem(position);
 
         titleTextView.setText(todo.gettName());
         descTextView.setText(todo.gettDesc());
+        Boolean isFeature = todo.getFeature();
+        if (isFeature = true) {
+            featureTextView.setVisibility(TextView.VISIBLE);
+        } else {
+            featureTextView.setVisibility(TextView.INVISIBLE);
+        }
+
+        Boolean isBug = todo.getBug();
+        if (isBug = true) {
+            bugTextView.setVisibility(TextView.VISIBLE);
+        } else {
+            bugTextView.setVisibility(TextView.INVISIBLE);
+        }
+
 
         return convertView;
     }
