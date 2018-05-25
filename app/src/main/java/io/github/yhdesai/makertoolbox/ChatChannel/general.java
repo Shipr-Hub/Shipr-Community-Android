1260d0f2663e7ad260ff213aac48fb1942e95923
Yash <yhdelectric@gmail.com>
2018-05-26 00:14:48 +0530

    added bottom nav bar

--- app/src/main/java/io/github/yhdesai/makertoolbox/ChatChannel/general.java
+++ app/src/main/java/io/github/yhdesai/makertoolbox/ChatChannel/general.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
---
+++ app/src/main/java/io/github/yhdesai/makertoolbox/ToolsList.java
@@ -0,0 +1,20 @@
+package io.github.yhdesai.makertoolbox;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ToolsList extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tools_list, container, false);
+    }
+
+}
