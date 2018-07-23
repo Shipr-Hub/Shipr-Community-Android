04352879aad60c4c0d108c6a517128cc55228347
yash <yhdelectric@gmail.com>
2018-07-23 15:53:01 +0530

    working on adding images to chat and profile

--- app/src/main/java/io/github/yhdesai/makertoolbox/Profile.java
+++ app/src/main/java/io/github/yhdesai/makertoolbox/Profile.java
@@ -1,6 +1,7 @@
 package io.github.yhdesai.makertoolbox;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,6 +21,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -35,9 +39,14 @@ public class Profile extends Fragment {
     private String email;
     private String profilePic;
     private Button mSubmitButton;
+    private ImageView mProfilePic;
 
 
     private FirebaseAuth mFirebaseAuth;
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference mProfileStroageReference;
+
+    private static final int RC_PHOTO_PICKER = 2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,11 +54,16 @@ public class Profile extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
         FirebaseApp.initializeApp(getActivity());
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseStorage = FirebaseStorage.getInstance();
+
+        mProfileStroageReference = mFirebaseStorage.getReference().child("profile_pic");
+
 
         pUsername = rootView.findViewById(R.id.usernameEditText);
         pDisplayName = rootView.findViewById(R.id.nameEditText);
         pEmail = rootView.findViewById(R.id.emailView);
         mSubmitButton = rootView.findViewById(R.id.submit);
+        mProfilePic = rootView.findViewById(R.id.profileImage);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
@@ -75,6 +89,19 @@ public class Profile extends Fragment {
         pUsername.setText(displayName);
         pEmail.setText(email);
         /*  pDisplayName.setText();*/
+
+        mProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                Log.d("ping", "Ping");
+                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+                Log.d("pong", "Pong");
+            }
+        });
+
 
 
         mSubmitButton.setOnClickListener(new View.OnClickListener()
@@ -102,6 +129,9 @@ public class Profile extends Fragment {
         });
         return rootView;
     }
+
+
+
 
 
 }
