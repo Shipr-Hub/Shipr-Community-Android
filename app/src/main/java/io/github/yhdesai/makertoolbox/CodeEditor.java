cfe6329aa2f4aee37d03726d4d24e56beb38189e
yash <yhdelectric@gmail.com>
2018-07-25 02:30:13 +0530

    removed codeviewer cause apparently you cant edit, will add it later when needed

--- app/src/main/java/io/github/yhdesai/makertoolbox/CodeEditor.java
+++ app/src/main/java/io/github/yhdesai/makertoolbox/CodeEditor.java
@@ -3,7 +3,6 @@ package io.github.yhdesai.makertoolbox;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import io.github.kbiakov.codeview.CodeView;
 
 public class CodeEditor extends AppCompatActivity {
 
@@ -11,7 +10,6 @@ public class CodeEditor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code_editor);
-        CodeView codeView = (CodeView) findViewById(R.id.code_view);
-        codeView.setCode(getString(R.string.listing_js), "js");
+
     }
 }
