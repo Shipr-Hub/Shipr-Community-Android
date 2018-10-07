6d47398db4dc553ae7f971aebf86f8aa60d8824d
Samuel-PC <requena244@gmail.com>
2018-10-07 19:26:00 +0200

    ASCIIViewer activity added

---
+++ app/src/main/java/io/github/yhdesai/makertoolbox/tools/ASCIIViewer.java
@@ -0,0 +1,15 @@
+package io.github.yhdesai.makertoolbox.tools;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.github.yhdesai.makertoolbox.R;
+
+public class ASCIIViewer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tool_asciiviewer);
+    }
+}
