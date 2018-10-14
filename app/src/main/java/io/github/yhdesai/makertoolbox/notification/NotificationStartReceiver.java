61bc0dbcf6e592e2b5c58edca48faf26120bf1cd
Parthav Patel <parthav46@gmail.com>
2018-10-14 22:48:22 +0530

    added notifications (#66)

---
+++ app/src/main/java/io/github/yhdesai/makertoolbox/notification/NotificationStartReceiver.java
@@ -0,0 +1,19 @@
+package io.github.yhdesai.makertoolbox.notification;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class NotificationStartReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        Intent service = new Intent(context, NotificationService.class);
+        if(Intent.ACTION_BOOT_COMPLETED.equals(action)){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) context.startForegroundService(service);
+            else context.startService(service);
+        }
+        else if(NotificationService.service_broadcast.equals(action) || Intent.ACTION_MY_PACKAGE_REPLACED.equals(action)) context.startService(service);
+    }
+}
