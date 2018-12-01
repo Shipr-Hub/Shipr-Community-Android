cf145eeff6cfc2173eb5e409e37d1334e2c9fefa
yash <yhdesai99@ymail.com>
2018-12-01 19:42:58 +0530

    working on chat channel spinner

--- app/src/main/java/tech/shipr/socialdev/Welcome_Screen.java
+++ app/src/main/java/tech/shipr/socialdev/Welcome_Screen.java
@@ -8,6 +8,6 @@ public class Welcome_Screen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome__screen);
+        setContentView(R.layout.activity_welcome_screen);
     }
 }
