4d52bb5119946e3af859c3f5d5926d4a9e03f76b
Yash Desai <yhdelectric@gmail.com>
2018-12-28 09:22:21 +0530

    edited MTActivity.java

--- app/src/main/java/tech/shipr/socialdev/view/MTActivity.java
+++ app/src/main/java/tech/shipr/socialdev/view/MTActivity.java
@@ -8,7 +8,6 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.Continuation;
