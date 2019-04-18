47c34d2d0da8d5d54ce380bf83c5f352c93249db
Shreyas Patil <shreyaspatilg@gmail.com>
2019-04-18 19:12:57 +0530

    Updated MessageAdapter - Added RecyclerView, Updated UI as #33, #94

--- app/src/main/java/tech/shipr/socialdev/adapter/MessageAdapter.java
+++ app/src/main/java/tech/shipr/socialdev/adapter/MessageAdapter.java
@@ -1,25 +1,24 @@
 package tech.shipr.socialdev.adapter;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.firebase.FirebaseApp;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -27,102 +26,127 @@ import tech.shipr.socialdev.R;
 import tech.shipr.socialdev.model.DeveloperMessage;
 import tech.shipr.socialdev.view.ViewProfileActivity;
 
-
-public class MessageAdapter extends ArrayAdapter<DeveloperMessage> {
+public abstract class MessageAdapter extends FirebaseRecyclerAdapter<DeveloperMessage, MessageAdapter.MessageViewHolder> {
 
     private final Context mContext;
+    private FirebaseUser mUser;
 
-    public MessageAdapter(Context context, int resource, List<DeveloperMessage> objects) {
-        super(context, resource, objects);
-        mContext = context;
+    private static final int MESSAGE_TYPE_RIGHT = 1;
+    private static final int MESSAGE_TYPE_LEFT = 2;
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public MessageAdapter(Context mContext, @NonNull FirebaseRecyclerOptions<DeveloperMessage> options) {
+        super(options);
+        this.mContext = mContext;
+
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        DeveloperMessage message = getItem(position);
+        assert mUser != null;
+
+        if (message.getUid() != null) {
+            if (message.getUid().equals(mUser.getUid())) {
+                return MESSAGE_TYPE_RIGHT;
+            } else {
+                return MESSAGE_TYPE_LEFT;
+            }
+        } else {
+            return MESSAGE_TYPE_LEFT;
+        }
+
     }
 
     @NonNull
     @Override
-    public View getView(int position, View convertView, @NotNull ViewGroup parent) {
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        initFirebase();
+        View messageView;
 
-        DeveloperMessage message = getItem(position);
-        assert message != null;
-
-
-        //TODO: refer issue #94
-       /* if (convertView == null) {
-            FirebaseUser user = firebaseAuth.getCurrentUser();
-            assert user != null;
-            String uid = user.getUid();
-            if (message.getUid().equals(uid)) {
-                convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message_right, parent, false);
-            } else {
-                convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message, parent, false);
-
-            }
-        }*/
-
-        if (convertView == null) {
-            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message, parent, false);
-        }
-
-
-        TextView messageTextView = convertView.findViewById(R.id.messageTextView);
-        messageTextView.setMaxWidth(mContext.getResources().getDisplayMetrics().widthPixels - 160);
-        TextView authorTextView = convertView.findViewById(R.id.nameTextView);
-        TextView timeTextView = convertView.findViewById(R.id.timeTextView);
-        //     TextView dateTextView = convertView.findViewById(R.id.dateTextView);
-        ImageView profileImageView = convertView.findViewById(R.id.photoImageView);
-
-
-        messageTextView.setVisibility(View.VISIBLE);
-        messageTextView.setText(message.getText());
-        authorTextView.setText(message.getName());
-
-        // Click listeners
-        profileImageView.setOnClickListener(new ClickHandler(mContext, message));
-
-
-        String pic = message.getProfilePic();
-
-        if (pic == null) {
-
-            Picasso.get()
-                    .load(R.drawable.ic_account_circle_black_36dp)
-                    .into(profileImageView);
+        if (viewType == MESSAGE_TYPE_RIGHT) {
+            messageView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message_right, parent, false);
         } else {
-            Picasso.get().load(pic).fit().into(profileImageView);
+            messageView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message, parent, false);
         }
 
-
-        String time = message.getTime();
-        Log.d("time", time);
-        try {
-            SimpleDateFormat mTime = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
-            mTime.setTimeZone(TimeZone.getTimeZone("IST"));
-            Date date = mTime.parse(time);
-            Log.d("date", date.toString());
-            //       dateTextView.setText(date.toString());
-            mTime.setTimeZone(TimeZone.getDefault());
-            String formattedTime = mTime.format(date);
-            timeTextView.setText(formattedTime);
-
-        } catch (java.text.ParseException e) {
-            e.printStackTrace();
-        }
-        return convertView;
+        return new MessageViewHolder(messageView);
     }
 
-    private void initFirebase() {
-        FirebaseApp.initializeApp(getContext());
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    @Override
+    protected void onBindViewHolder(@NonNull MessageViewHolder holder, int position, @NonNull DeveloperMessage model) {
+        holder.setMessage(model);
+        holder.profileImageView.setOnClickListener(new ClickHandler(model));
+    }
+
+    @Override
+    public void onDataChanged() {
+        super.onDataChanged();
+        onLoaded();
+    }
+
+    public abstract void onLoaded();
+
+    public static class MessageViewHolder extends RecyclerView.ViewHolder {
+
+        TextView messageTextView;
+        TextView authorTextView;
+        TextView timeTextView;
+        ImageView profileImageView;
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            messageTextView = itemView.findViewById(R.id.messageTextView);
+            authorTextView = itemView.findViewById(R.id.nameTextView);
+            timeTextView  = itemView.findViewById(R.id.timeTextView);
+            profileImageView = itemView.findViewById(R.id.photoImageView);
+            messageTextView.setMaxWidth(itemView.getResources().getDisplayMetrics().widthPixels - 160);
+        }
+
+        public void setMessage(DeveloperMessage message) {
+            messageTextView.setText(message.getText());
+            authorTextView.setText(message.getName());
+            timeTextView.setText(message.getTime());
+
+            if (message.getProfilePic() == null) {
+
+                Picasso.get()
+                        .load(R.drawable.ic_account_circle_black_36dp)
+                        .into(profileImageView);
+            } else {
+                Picasso.get().load(message.getProfilePic()).fit().into(profileImageView);
+            }
+
+            String time = message.getTime();
+            Log.d("time", time);
+            try {
+                SimpleDateFormat mTime = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
+                mTime.setTimeZone(TimeZone.getTimeZone("IST"));
+                Date date = mTime.parse(time);
+                Log.d("date", date.toString());
+                //       dateTextView.setText(date.toString());
+                mTime.setTimeZone(TimeZone.getDefault());
+                String formattedTime = mTime.format(date);
+                timeTextView.setText(formattedTime);
+
+            } catch (java.text.ParseException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     class ClickHandler implements View.OnClickListener {
 
-        private final Context mContext;
         private final DeveloperMessage message;
 
-        ClickHandler(Context mContext, DeveloperMessage message) {
-            this.mContext = mContext;
+        ClickHandler(DeveloperMessage message) {
             this.message = message;
         }
 
@@ -138,6 +162,4 @@ public class MessageAdapter extends ArrayAdapter<DeveloperMessage> {
             }
         }
     }
-
-
 }
